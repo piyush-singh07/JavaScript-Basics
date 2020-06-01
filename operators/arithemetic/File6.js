7744ae405d3545b5d3c8d95834bb1674609f0a54
Piyush Singh <Piyush.Singh@utc.com>
2020-06-01 23:46:24 +0530

    added other operators

---
+++ operators/arithemetic/File6.js
@@ -0,0 +1,30 @@
+/* Boolean with arithemetic operation
+true = 1
+false = 0
+
+if boolean and arithmetic operator with number----> will always be number
+if boolean + string will------> be string */
+
+var a = true
+var b = 10 + a
+console.log(b) //11
+
+var c = "10" + a
+console.log(c) //10true
+
+var d = 10 - a
+var e = 10 * a
+var f = 10 / a
+var g = 10 % a
+
+console.log(d)
+console.log(e)
+console.log(f)
+console.log(g)
+
+var h = false;
+
+console.log(a + a)
+console.log(a + h)
+console.log(h + a)
+console.log(h + h)
