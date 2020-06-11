3e1636b7e4077d545d5e03e1b0068bc40ec51484
Piyush Singh <Piyush.Singh@utc.com>
2020-06-11 23:36:17 +0530

    added a new folder

---
+++ operators/equal_double_equal/File1.js
@@ -0,0 +1,26 @@
+/*
+== --> compares the content
+=== --> compares content as well as type checking
+*/
+console.log(undefined == undefined)
+console.log(undefined === undefined)
+console.log(null == null)
+console.log(null === null)
+console.log(null == undefined)
+console.log(null === undefined)
+
+console.log(10 == "10")
+console.log(10 == "ten")
+console.log(10 === "10")
+
+console.log(10 == 10)
+console.log(10 === 10)
+
+
+
+
+
+// concept:  for arithematic calculation of fractions in JS  takes 64 bit precision
+
+console.log(0.1 + 0.2);
+console.log(0.4 - 0.3)
